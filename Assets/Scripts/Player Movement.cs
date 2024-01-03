3cd7e9ebbb3e78643b975fe2607c3eba7f3999d0
Kyehor1990 <ardauludag2005@gmail.com>
2024-01-03 22:22:04 +0300

    playermovement_gravity

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,11 +1,23 @@
-using System.Collections;
+ using System;
+ using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+ using Unity.VisualScripting;
+ using UnityEngine;
+ using UnityEngine.Serialization;
 
-public class PlayerMovement : MonoBehaviour
+ public class PlayerMovement : MonoBehaviour
 {
-    private float movementspeed = 2.0f;
+    public Rigidbody2D Me;
+    [FormerlySerializedAs("Flip")] public SpriteRenderer hokkabaz;
+    
+    private float movementspeed = 6.0f;
     private float jump = 5.0f;
+
+    private void Start()
+    {
+        Me = GetComponent<Rigidbody2D>();
+        hokkabaz = GetComponent<SpriteRenderer>();
+    }
 
 
     // Update is called once per frame
@@ -26,5 +38,12 @@ public class PlayerMovement : MonoBehaviour
         transform.Translate(Vector3.up * jump * Time.deltaTime);
         }
         
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Me.gravityScale *= -1;
+
+            hokkabaz.flipY = true;
+            
+        }
     }
 }
