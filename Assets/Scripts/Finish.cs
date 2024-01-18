1d38ee8a4eb2c97ee4204aa8d56f579b74228695
Sklpp3r <155382339+Sklpp3r@users.noreply.github.com>
2024-01-18 23:01:34 +0300

    Fix 3

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+   
+}
